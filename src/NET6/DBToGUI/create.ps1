3f4e1100339307ed00d41a2374f0ab75dc3a039f
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-01 12:20:16 +0200

    no imodels

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -1,8 +1,5 @@
 dotnet tool restore
-while($true){
 cd .\FastDBToGUI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
-dotnet ef dbcontext scaffold "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --use-database-names --context OriginalDbContext
+dotnet ef dbcontext scaffold "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --use-database-names --context OriginalDbContext --namespace FastDBToGuiModels  --output-dir ../FastDBToGuiIModels
 cd ..
-pause
-}
