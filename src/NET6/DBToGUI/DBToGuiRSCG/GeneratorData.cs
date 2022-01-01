91720b78bc76d1561abd9dc50ff7f4063532904b
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-01 18:41:48 +0200

    added generator

---
+++ src/NET6/DBToGUI/DBToGuiRSCG/GeneratorData.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DBToGuiRSCG;
+
+[Generator]
+public class GeneratorData : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
