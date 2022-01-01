49d5577835230e61ad6f2270275dcfe27bdbf686
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-01 11:54:18 +0200

    ref

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -1,5 +1,8 @@
 dotnet tool restore
+while($true){
 cd .\FastDBToGUI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
 dotnet ef dbcontext scaffold "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --use-database-names --context OriginalDbContext
 cd ..
+pause
+}
