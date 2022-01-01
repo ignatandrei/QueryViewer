d769a303341723b644b22501c9ecd93663482add
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-01 14:18:09 +0200

    webapi

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -1,8 +1,9 @@
 cls
 $connection= "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>"
+$connection="ConnectionStrings:defaultConnection"
 $provider = "Microsoft.EntityFrameworkCore.SqlServer"
 dotnet tool restore
-cd .\FastDBToGUIContext\
+cd .\FastDBToGUIWebAPI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
-dotnet ef dbcontext scaffold $connection $provider --data-annotations --use-database-names --context OriginalDbContext --context-namespace FastDBToGuiContext --namespace FastDBToGuiModels --context-dir . --output-dir ../FastDBToGuiModels
+dotnet ef dbcontext scaffold Name=$connection $provider --data-annotations --use-database-names --context OriginalDbContext --context-namespace FastDBToGuiContext --namespace FastDBToGuiModels --context-dir ../FastDBToGuiContext/ --output-dir ../FastDBToGuiModels/
 cd ..
