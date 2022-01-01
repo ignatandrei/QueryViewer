a05aa5e69c686416b1786cbd628a5af246195a82
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-01 12:24:26 +0200

    vars

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -1,5 +1,7 @@
+$connection= "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>"
+$provider = "Microsoft.EntityFrameworkCore.SqlServer"
 dotnet tool restore
 cd .\FastDBToGUI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
-dotnet ef dbcontext scaffold "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --use-database-names --context OriginalDbContext --namespace FastDBToGuiModels  --output-dir ../FastDBToGuiIModels
+dotnet ef dbcontext scaffold $connection $provider --data-annotations --use-database-names --context OriginalDbContext --namespace FastDBToGuiModels  --output-dir ../FastDBToGuiModels
 cd ..
