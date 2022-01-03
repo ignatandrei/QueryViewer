803f06ee1ee4f364209c50099a312dda71677736
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-03 23:41:57 +0200

    skinny

--- src/NET6/DBToGUI/FastDBToGuiWebAPI/Controllers/AppDbContextController.cs
+++ src/NET6/DBToGUI/FastDBToGuiWebAPI/Controllers/AppDbContextController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SkinnyControllersCommon;
 
 namespace FastDBToGuiWebAPI.Controllers
 {
+    [AutoActions(template = TemplateIndicator.AllPost, FieldsName = new[] { "*" }, ExcludeFields = new[] { "_logger" })]
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class AppDbContextController : ControllerBase
+    public partial class AppDbContextController : ControllerBase
     {
         private readonly ApplicationDbContext context;
 
@@ -13,26 +15,26 @@ namespace FastDBToGuiWebAPI.Controllers
         {
             this.context = context;
         }
-        [HttpGet]
-        public long GetDeps()
-        {
-            return context.Department.LongCount();
-        }
-        [HttpPost]
-        public Task<long> DepartmentFindNumber(SearchDepartment search)
-        {
-            return context.DepartmentFindNumber(search);
+        //[HttpGet]
+        //public long GetDeps()
+        //{
+        //    return context.Department.LongCount();
+        //}
+        //[HttpPost]
+        //public Task<long> DepartmentFindNumber(SearchDepartment search)
+        //{
+        //    return context.DepartmentFindNumber(search);
             
-        }
-        [HttpGet]
-        public async IAsyncEnumerable<Department> DepartmentAsync()
-        {
-            await foreach(var item in context.DepartmentFindAsync(null))
-            {
-                await Task.Delay(2000);
-                yield return item;
-            }
+        //}
+        //[HttpGet]
+        //public async IAsyncEnumerable<Department> DepartmentAsync()
+        //{
+        //    await foreach(var item in context.DepartmentFindAsync(null))
+        //    {
+        //        await Task.Delay(2000);
+        //        yield return item;
+        //    }
 
-        }
+        //}
     }
 }
