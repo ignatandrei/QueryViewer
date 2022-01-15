210174e5d238a27a73abc1458c1478ede01cd45d
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-15 17:32:06 +0200

    start console

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -12,4 +12,6 @@ cd .\FastDBToGUIWebAPI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
 dotnet ef dbcontext scaffold $connection $provider -v -f --no-pluralize --use-database-names  --no-onconfiguring --context ApplicationDbContext  --context-namespace Generated --namespace Generated --context-dir ../FastDBToGuiContext/Generated/ --output-dir ../FastDBToGuiModels/Generated/  --prefix-output --json
 cd ..
+dotnet dotnet-cs2ts FastDBToGuiModels\Generated -o DBToGuiAng\src\Models
 dotnet build
+
