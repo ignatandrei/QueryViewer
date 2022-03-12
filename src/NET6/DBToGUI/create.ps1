b674dad2a5280eb22ea803c71f7b3c03f0ea0d2d
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-12 23:13:59 +0200

    eliminate ts

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -2,7 +2,7 @@ cls
 $appSettings = Get-Content -Raw -Path FastDBToGuiWebAPI/appSettings.json | ConvertFrom-Json
 
 Write-host $appSettings.ConnectionStrings.DefaultConnection
-#return
+
 
 #$connection= "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>"
 $connection=$appSettings.ConnectionStrings.DefaultConnection
@@ -11,7 +11,7 @@ dotnet tool restore
 cd .\FastDBToGUIWebAPI\
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
 dotnet ef dbcontext scaffold $connection $provider -v -f --no-pluralize --use-database-names  --no-onconfiguring --context ApplicationDbContext  --context-namespace Generated --namespace Generated --context-dir ../FastDBToGuiContext/Generated/ --output-dir ../FastDBToGuiModels/Generated/  --prefix-output --json
-cd ..
-dotnet dotnet-cs2ts FastDBToGuiModels\Generated -o DBToGuiAng\src\Models
+#cd ..
+#dotnet dotnet-cs2ts FastDBToGuiModels\Generated -o DBToGuiAng\src\Models
 dotnet build
 
