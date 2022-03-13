71ea12be2ae12981dad12fdd11d2d2fc739a0d25
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-13 13:00:27 +0200

    now with variables

--- src/NET6/DBToGUI/create.ps1
+++ src/NET6/DBToGUI/create.ps1
@@ -1,16 +1,21 @@
+$pathToConnectionStringInAppSettings = "../FastDBToGuiWebAPI/appSettings.json"
+$pathToWebAPI = "../FastDBToGUIWebAPI" 
+$pathToContext="../FastDBToGuiContext/Generated/"
+$pathToModels="../FastDBToGuiModels/Generated/"
 cls
-$appSettings = Get-Content -Raw -Path FastDBToGuiWebAPI/appSettings.json | ConvertFrom-Json
+$appSettings = Get-Content -Raw -Path $pathToConnectionStringInAppSettings | ConvertFrom-Json
 
-Write-host $appSettings.ConnectionStrings.DefaultConnection
+Write-host "initiate for " $appSettings.ConnectionStrings.DefaultConnection
 
 
 #$connection= "Data Source=.;Initial Catalog=TestData;UId=sa;pwd=<YourStrong@Passw0rd>"
 $connection=$appSettings.ConnectionStrings.DefaultConnection
 $provider = "Microsoft.EntityFrameworkCore.SqlServer"
+
 dotnet tool restore
-cd .\FastDBToGUIWebAPI\
+cd $pathToWebAPI
 #https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
-dotnet ef dbcontext scaffold $connection $provider -v -f --no-pluralize --use-database-names  --no-onconfiguring --context ApplicationDbContext  --context-namespace Generated --namespace Generated --context-dir ../FastDBToGuiContext/Generated/ --output-dir ../FastDBToGuiModels/Generated/  --prefix-output --json
+dotnet ef dbcontext scaffold $connection $provider -v -f --no-pluralize --use-database-names  --no-onconfiguring --context ApplicationDbContext  --context-namespace Generated --namespace Generated --context-dir $pathToContext  --output-dir $pathToModels  --prefix-output --json
 #cd ..
 #dotnet dotnet-cs2ts FastDBToGuiModels\Generated -o DBToGuiAng\src\Models
 dotnet build
