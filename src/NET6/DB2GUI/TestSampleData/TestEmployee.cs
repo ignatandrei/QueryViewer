12503938d10598d316875f1fa30858fe7a4e62b8
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-26 09:46:55 +0200

    more starts

---
+++ src/NET6/DB2GUI/TestSampleData/TestEmployee.cs
@@ -0,0 +1,43 @@
+﻿namespace TestSampleData;
+
+public class TestEmployee
+{
+    void CreateDb()
+    {
+        var context = this.context;
+        var file = File.ReadAllText("insertPubs.sql").Split("GO", StringSplitOptions.RemoveEmptyEntries);
+        foreach (var item in file)
+        {
+            try
+            {
+                context.Database.ExecuteSqlRaw(item);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(item, ex);
+
+            }
+        }
+
+    }
+    ApplicationDbContext context
+    {
+        get
+        {
+            return new ApplicationDbContext();
+
+        }
+    }
+
+    [Theory] 
+    [InlineData(SearchCriteria.Equal, eemployeeColumns.minit, "M", 3)]
+    [InlineData(SearchCriteria.Less, eemployeeColumns.minit, "M", 29)]
+    [InlineData(SearchCriteria.Greater, eemployeeColumns.minit, "M", 11)]
+    public async Task SearchAdvanced(SearchCriteria sc, eemployeeColumns col, string val, int nrRecs)
+    {
+        //CreateDb();
+        var data = await context.employeeSimpleSearch(sc, col, val).ToArrayAsync();
+        Assert.Equal(nrRecs, data.Length);
+    }
+
+}
