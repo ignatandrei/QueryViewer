2f9b57f3d6d8a400e35533ea27eaef96eeb55fb0
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-26 11:20:50 +0200

    with dates

--- src/NET6/DB2GUI/TestSampleData/TestEmployee.cs
+++ src/NET6/DB2GUI/TestSampleData/TestEmployee.cs
@@ -30,9 +30,9 @@ public class TestEmployee
     }
     static TestEmployee()
     {
-        CreateDb();
+        //CreateDb();
     }
-    [Theory]
+    [Theory] 
     [InlineData(SearchCriteria.Equal, eemployeeColumns.minit, "M", 3)]
     [InlineData(SearchCriteria.Less, eemployeeColumns.minit, "M", 29)]
     [InlineData(SearchCriteria.Greater, eemployeeColumns.minit, "M", 11)]
@@ -44,6 +44,11 @@ public class TestEmployee
     [InlineData(SearchCriteria.Equal, eemployeeColumns.job_lvl, "87", 1)]
     [InlineData(SearchCriteria.Less, eemployeeColumns.job_lvl, "87", 10)]
     [InlineData(SearchCriteria.Greater, eemployeeColumns.job_lvl, "87", 32)]
+    [InlineData(SearchCriteria.Equal, eemployeeColumns.hire_date, "1993-08-19", 1)]
+    [InlineData(SearchCriteria.Less, eemployeeColumns.hire_date, "1993-08-19", 37)]
+    [InlineData(SearchCriteria.LessOrEqual, eemployeeColumns.hire_date, "1993-08-19", 38)]
+    [InlineData(SearchCriteria.Greater, eemployeeColumns.hire_date, "1993-08-19", 5)]
+    [InlineData(SearchCriteria.GreaterOrEqual, eemployeeColumns.hire_date, "1993-08-19", 6)]
 
     public async Task SearchAdvanced(SearchCriteria sc, eemployeeColumns col, string val, int nrRecs)
     {
