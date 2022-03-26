13a23cfe591f06a9c5ae92e238a3aaaf26402d6c
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-26 20:03:54 +0200

    adding float

--- src/NET6/DB2GUI/TestSampleData/TestOrderDetail.cs
+++ src/NET6/DB2GUI/TestSampleData/TestOrderDetail.cs
@@ -34,7 +34,9 @@ public class TestOrderDetail
     }
 
     [Theory] 
-    [InlineData(SearchCriteria.Equal, eOrder_DetailsColumns.Quantity, "8042", 1)]
+    [InlineData(SearchCriteria.Equal, eOrder_DetailsColumns.Quantity, "8042", 0)]
+    [InlineData(SearchCriteria.Equal, eOrder_DetailsColumns.UnitPrice, "8042", 0)]
+    [InlineData(SearchCriteria.Equal, eOrder_DetailsColumns.Discount, "8042", 0)]
     public async Task SearchAdvanced(SearchCriteria sc, eOrder_DetailsColumns col, string val, int nrRecs)
     {
         CreateDb();
