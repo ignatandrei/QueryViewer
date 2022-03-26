f9cf61c4153048ad712f559dafb8d3dee423bba7
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-26 17:09:35 +0200

    with date time

--- src/NET6/DB2GUI/ExampleContext/ApplicationDbContext.cs
+++ src/NET6/DB2GUI/ExampleContext/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ public partial class ApplicationDbContext :DbContext
 {
     void x()
     {
-        EF.Functions.DateDiffYear()
+        //EF.Functions.DateDiffYear();
         //EF.Functions.Like()
         //Microsoft.EntityFrameworkCore.DbFunctionsExtensions.Like(DbFunctions,"asd", "asdasd");
     }   
--- src/NET6/DB2GUI/TestSampleData/TestEmployee.cs
+++ src/NET6/DB2GUI/TestSampleData/TestEmployee.cs
@@ -32,7 +32,7 @@ public class TestEmployee
     {
         //CreateDb();
     }
-    [Theory] 
+    [Theory]
     [InlineData(SearchCriteria.Equal, eemployeeColumns.minit, "M", 3)]
     [InlineData(SearchCriteria.Less, eemployeeColumns.minit, "M", 29)]
     [InlineData(SearchCriteria.Greater, eemployeeColumns.minit, "M", 11)]
@@ -49,6 +49,18 @@ public class TestEmployee
     [InlineData(SearchCriteria.LessOrEqual, eemployeeColumns.hire_date, "1993-08-19", 38)]
     [InlineData(SearchCriteria.Greater, eemployeeColumns.hire_date, "1993-08-19", 5)]
     [InlineData(SearchCriteria.GreaterOrEqual, eemployeeColumns.hire_date, "1993-08-19", 6)]
+    [InlineData(SearchCriteria.EqualYear, eemployeeColumns.hire_date, "1993-08-19", 7)]
+    [InlineData(SearchCriteria.DifferentYear, eemployeeColumns.hire_date, "1993-08-19", 36)]
+    [InlineData(SearchCriteria.GreaterYear, eemployeeColumns.hire_date, "1993-08-19", 3)]
+    [InlineData(SearchCriteria.GreaterOrEqualYear, eemployeeColumns.hire_date, "1993-08-19", 10)]
+    [InlineData(SearchCriteria.LessYear, eemployeeColumns.hire_date, "1993-08-19", 33)]
+    [InlineData(SearchCriteria.LessOrEqualYear, eemployeeColumns.hire_date, "1993-08-19", 40)]
+    [InlineData(SearchCriteria.EqualMonthYear, eemployeeColumns.hire_date, "1993-08-19", 1)]
+    [InlineData(SearchCriteria.DifferentMonthYear, eemployeeColumns.hire_date, "1993-08-19", 42)]
+    [InlineData(SearchCriteria.GreaterMonthYear, eemployeeColumns.hire_date, "1993-08-19", 37)]
+    [InlineData(SearchCriteria.GreaterOrEqualMonthYear, eemployeeColumns.hire_date, "1993-08-19", 38)]
+    [InlineData(SearchCriteria.LessMonthYear, eemployeeColumns.hire_date, "1993-08-19", 5)]
+    [InlineData(SearchCriteria.LessOrEqualMonthYear, eemployeeColumns.hire_date, "1993-08-19", 6)]
 
     public async Task SearchAdvanced(SearchCriteria sc, eemployeeColumns col, string val, int nrRecs)
     {
