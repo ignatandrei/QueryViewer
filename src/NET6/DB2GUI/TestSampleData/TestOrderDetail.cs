9093032ffff449796208d5a397247c6ec0772920
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-26 19:55:18 +0200

    modified sql

--- src/NET6/DB2GUI/TestSampleData/TestOrderDetail.cs
+++ src/NET6/DB2GUI/TestSampleData/TestOrderDetail.cs
@@ -24,34 +24,21 @@ public class TestOrderDetail
         }
 
     }
-    static PubsContext context
+    static NorthwindContext context
     {
         get
         {
-            return new PubsContext();
+            return new NorthwindContext();
 
         }
     }
 
-    [Theory]
-    [InlineData(SearchCriteria.Equal, ediscountsColumns.stor_id, "8042", 1)]
-    [InlineData(SearchCriteria.Different, ediscountsColumns.stor_id, "8042", 2)]
-    [InlineData(SearchCriteria.Equal, ediscountsColumns.stor_id, null, 2)]
-    [InlineData(SearchCriteria.Different, ediscountsColumns.stor_id, null, 1)]
-    [InlineData(SearchCriteria.Equal, ediscountsColumns.discount, "5", 1)]
-    [InlineData(SearchCriteria.Less, ediscountsColumns.discount, "5", 0)]
-    [InlineData(SearchCriteria.LessOrEqual, ediscountsColumns.discount, "5", 1)]
-    [InlineData(SearchCriteria.Less, ediscountsColumns.discount, "10.5", 2)]
-    [InlineData(SearchCriteria.LessOrEqual, ediscountsColumns.discount, "10.5", 3)]
-    [InlineData(SearchCriteria.Equal, ediscountsColumns.lowqty, "100", 1)]
-    [InlineData(SearchCriteria.Less, ediscountsColumns.lowqty, "100", 0)]
-    [InlineData(SearchCriteria.LessOrEqual, ediscountsColumns.lowqty, "100", 1)]
-    [InlineData(SearchCriteria.Greater, ediscountsColumns.lowqty, "100", 0)]
-    [InlineData(SearchCriteria.GreaterOrEqual, ediscountsColumns.lowqty, "100", 1)]
-    public async Task SearchAdvanced(SearchCriteria sc, ediscountsColumns col, string val, int nrRecs)
+    [Theory] 
+    [InlineData(SearchCriteria.Equal, eOrder_DetailsColumns.Quantity, "8042", 1)]
+    public async Task SearchAdvanced(SearchCriteria sc, eOrder_DetailsColumns col, string val, int nrRecs)
     {
         CreateDb();
-        var data = await context.discountsSimpleSearch(sc, col, val).ToArrayAsync();
+        var data = await context.Order_DetailsSimpleSearch(sc, col, val).ToArrayAsync();
         Assert.Equal(nrRecs, data.Length);
     }
 
