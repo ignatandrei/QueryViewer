d932458c8a1f1dd321b1ff643e3bcc2646b7c4bf
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-29 02:03:01 +0300

    new version

--- src/NET6/DB2GUI/DB2GUITemplate/createAll.ps1
+++ src/NET6/DB2GUI/DB2GUITemplate/createAll.ps1
@@ -5,9 +5,12 @@ Expand-Archive ./GeneratorFromDB.zip -DestinationPath GeneratorFromDB
 Remove-Item -Path GeneratorFromDB\ExampleContext\context.txt -Force
 Remove-Item -Path GeneratorFromDB\ExampleModels\models.txt -Force
 Remove-Item -Path GeneratorFromDB\ExampleWebAPI\controller.txt -Force
+Remove-Item -Path GeneratorFromDB\GeneratorFromDB\create.ps1 -Force
 Copy-Item ..\DB2GUI\Templates\context.txt -Destination GeneratorFromDB\ExampleContext\context.txt   
 Copy-Item ..\DB2GUI\Templates\models.txt -Destination GeneratorFromDB\ExampleModels\models.txt 
 Copy-Item ..\DB2GUI\Templates\controller.txt -Destination GeneratorFromDB\ExampleWebAPI\controller.txt 
+Copy-Item ..\GeneratorFromDB\create.ps1 -Destination GeneratorFromDB\GeneratorFromDB\create.ps1
+
 Compress-Archive -DestinationPath .\a -Path GeneratorFromDB\*
 Remove-Item .\GeneratorFromDB.zip
 Remove-Item .\ProjectTemplates\GeneratorFromDB.zip
