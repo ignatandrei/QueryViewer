dcbe2550022f9e73613271a88b69f1015ebbbcdb
Andrei Ignat <ignatandrei@yahoo.com>
2022-03-29 20:51:16 +0300

    modified names

---
+++ src/NET6/DB2GUI/ExampleModels/GeneratedPubs/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Generated
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employee = new HashSet<Employee>();
+        }
+
+        [Key]
+        public long IDDepartment { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Name { get; set; } = null!;
+        public int? TenantID { get; set; }
+
+        [InverseProperty("IDDepartmentNavigation")]
+        public virtual ICollection<Employee> Employee { get; set; }
+    }
+}
---
+++ src/NET6/DB2GUI/ExampleModels/GeneratedPubs/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Generated
+{
+    public partial class Employee
+    {
+        [Key]
+        public long IDEmployee { get; set; }
+        [StringLength(100)]
+        [Unicode(false)]
+        public string Name { get; set; } = null!;
+        public long IDDepartment { get; set; }
+
+        [ForeignKey("IDDepartment")]
+        [InverseProperty("Employee")]
+        public virtual Department IDDepartmentNavigation { get; set; } = null!;
+    }
+}
