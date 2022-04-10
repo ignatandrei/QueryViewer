0e54d3a02bad0c47fb8af02a092fa7f467464f5d
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-11 01:33:36 +0300

    adding refernce

--- src/NET6/DB2GUI/DB2GUITemplate/createAll.ps1
+++ src/NET6/DB2GUI/DB2GUITemplate/createAll.ps1
@@ -1,7 +1,7 @@
 cls
 Remove-Item -Path .\a.zip -Force -Recurse -ErrorAction SilentlyContinue 
-Remove-Item -Path .\GeneratorFromDB -Force -Recurse -ErrorAction SilentlyContinue 
-Expand-Archive ./GeneratorFromDB.zip -DestinationPath GeneratorFromDB
+# Remove-Item -Path .\GeneratorFromDB -Force -Recurse -ErrorAction SilentlyContinue 
+# Expand-Archive ./GeneratorFromDB.zip -DestinationPath GeneratorFromDB
 Remove-Item -Path GeneratorFromDB\ExampleContext\context.txt -Force
 Remove-Item -Path GeneratorFromDB\ExampleModels\models.txt -Force
 Remove-Item -Path GeneratorFromDB\ExampleWebAPI\controller.txt -Force
