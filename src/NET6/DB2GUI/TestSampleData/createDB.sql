4169ceba586d9f2bc03e71485bf81cd2c97f105f
Andrei Ignat <ignatandrei@yahoo.com>
2023-02-05 10:50:18 +0200

    added generated

---
+++ src/NET6/DB2GUI/TestSampleData/createDB.sql
@@ -0,0 +1,86 @@
+/*
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" `
+   -p 1433:1433 --name sql1 --hostname sql1 `
+   -d `
+   mcr.microsoft.com/mssql/server:2022-latest
+
+dotnet ef dbcontext scaffold "Trusted_Connection=Yes;Data Source=.;Initial Catalog=tests;UID=sa;PWD=<YourStrong@Passw0rd>;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer --force --no-onconfiguring --no-pluralize --no-build --json
+
+*/
+create database tests
+go
+
+USE [tests]
+GO
+create table test(
+id int,
+name varchar(200)
+)
+/****** Object:  ForeignKey [FK_Employee_Department]    Script Date: 07/05/2010 01:50:15 ******/
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employee_Department]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employee]'))
+ALTER TABLE [dbo].[Employee] DROP CONSTRAINT [FK_Employee_Department]
+GO
+/****** Object:  Table [dbo].[Employee]    Script Date: 07/05/2010 01:50:15 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Employee]') AND type in (N'U'))
+DROP TABLE [dbo].[Employee]
+GO
+/****** Object:  Table [dbo].[Department]    Script Date: 07/05/2010 01:50:15 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Department]') AND type in (N'U'))
+DROP TABLE [dbo].[Department]
+GO
+/****** Object:  Table [dbo].[Department]    Script Date: 07/05/2010 01:50:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Department]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Department](
+	[IDDepartment] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED 
+(
+	[IDDepartment] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+SET IDENTITY_INSERT [dbo].[Department] ON
+INSERT [dbo].[Department] ([IDDepartment], [Name]) VALUES (2, N'IT')
+INSERT [dbo].[Department] ([IDDepartment], [Name]) VALUES (3, N'HR')
+SET IDENTITY_INSERT [dbo].[Department] OFF
+/****** Object:  Table [dbo].[Employee]    Script Date: 07/05/2010 01:50:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Employee]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Employee](
+	[IDEmployee] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[IDDepartment] [bigint] NOT NULL,
+	[Salary] [bigint] NOT NULL
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[IDEmployee] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  ForeignKey [FK_Employee_Department]    Script Date: 07/05/2010 01:50:15 ******/
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employee_Department]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employee]'))
+ALTER TABLE [dbo].[Employee]  WITH CHECK ADD  CONSTRAINT [FK_Employee_Department] FOREIGN KEY([IDDepartment])
+REFERENCES [dbo].[Department] ([IDDepartment])
+GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employee_Department]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employee]'))
+ALTER TABLE [dbo].[Employee] CHECK CONSTRAINT [FK_Employee_Department]
+GO
---
+++ src/NET7/DB2GUI/GeneratorFromDB/DBMeta.cs
@@ -0,0 +1,91 @@
+﻿namespace GeneratorFromDB;
+
+public record AllDB
+{
+    private AllDB()
+    {
+
+    }
+    public static AllDB Singleton = new();
+    private Dictionary<string, MetaDB> data = new();
+    public MetaDB[] DBs
+    {
+        get
+        {
+            return data.Values.ToArray();
+        }
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddDb)}");
+        }
+    }
+    public string[] DBNames
+    {
+        get { return data.Keys.ToArray(); }
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddDb)}");
+        }
+    }
+    public void AddDb(MetaDB item)
+    {
+        data.Add(item.Name, item);
+    }
+}
+public record MetaDB(string Name)
+{
+    private Dictionary<string, MetaTable> data = new(); 
+    public MetaTable[] Tables
+    {
+        get
+        {
+            return data.Values.ToArray();
+        }
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddTable)}");
+        }
+    }
+    public string[] TableNames
+    {
+        get { return data.Keys.ToArray();}
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddTable)}");
+        }
+    }
+    public void AddTable(MetaTable table)
+    {
+        data.Add(table.Name, table);
+    }
+}
+public record MetaTable(string Name)
+{
+    private Dictionary<string, MetaColumn> data = new();
+    public MetaColumn[] Columns
+    {
+        get
+        {
+            return data.Values.ToArray();
+        }
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddColumn)}");
+        }
+    }
+    public string[] ColumnsNames
+    {
+        get { return data.Keys.ToArray(); }
+        set
+        {
+            throw new ArgumentException($"use {nameof(AddColumn)}");
+        }
+    }
+    public void AddColumn(MetaColumn item)
+    {
+        data.Add(item.Name, item);
+    }
+}
+public record MetaColumn(string Name, string Type, bool IsNullable)
+{
+};
