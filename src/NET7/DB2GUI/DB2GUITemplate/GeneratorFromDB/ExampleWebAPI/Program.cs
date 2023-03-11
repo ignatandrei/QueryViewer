86211cb8bdb125bd958021c983a974434eaf38f9
Andrei Ignat <ignatandrei@yahoo.com>
2023-03-11 07:18:27 +0200

    starting create template

---
+++ src/NET7/DB2GUI/DB2GUITemplate/GeneratorFromDB/ExampleWebAPI/Program.cs
@@ -0,0 +1,52 @@
+using AMSWebAPI;
+using Generated;
+using Microsoft.EntityFrameworkCore;
+using NetCore2BlocklyNew;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextFactory<ApplicationDBContext>(
+
+        options =>
+        {
+            var cn = builder.Configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(cn);
+        }
+     )
+   ;
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseBlocklyUI(app.Environment);
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseAMS();
+app.UseBlocklyAutomation();
+app.Run();
--- src/NET7/DB2GUI/DB2GUI_RSCG/globalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.CodeAnalysis;
-global using System;
-global using Newtonsoft.Json;
-global using System.Collections.Generic;
-global using System.Text;
-global using System.Linq;
