2976ad79a1be6a28de480e9801d2965d0cf80ba5
Andrei Ignat <ignatandrei@yahoo.com>
2023-03-11 18:54:02 +0200

    start generating

--- src/NET7/DB2GUI/DB2GUITemplate/createAll.ps1
+++ src/NET7/DB2GUI/DB2GUITemplate/createAll.ps1
@@ -1,17 +1,60 @@
 cls
+Write-Host 'starting'
 Remove-Item -Path .\a.zip -Force -Recurse -ErrorAction SilentlyContinue 
-# Remove-Item -Path .\GeneratorFromDB -Force -Recurse -ErrorAction SilentlyContinue 
-# Expand-Archive ./GeneratorFromDB.zip -DestinationPath GeneratorFromDB
-Remove-Item -Path GeneratorFromDB\ExampleContext\context.txt -Force
-Remove-Item -Path GeneratorFromDB\ExampleModels\models.txt -Force
-Remove-Item -Path GeneratorFromDB\ExampleWebAPI\controller.txt -Force
-Remove-Item -Path GeneratorFromDB\GeneratorFromDB\create.ps1 -Force
-Copy-Item ..\DB2GUI\Templates\context.txt -Destination GeneratorFromDB\ExampleContext\context.txt   
-Copy-Item ..\DB2GUI\Templates\models.txt -Destination GeneratorFromDB\ExampleModels\models.txt 
-Copy-Item ..\DB2GUI\Templates\controller.txt -Destination GeneratorFromDB\ExampleWebAPI\controller.txt 
-Copy-Item ..\GeneratorFromDB\create.ps1 -Destination GeneratorFromDB\GeneratorFromDB\create.ps1
+Remove-Item -Path .\GeneratorFromDBTemp -Force -Recurse -ErrorAction SilentlyContinue 
+$FileLocation = "source.extension.vsixmanifest"
+[xml]$xmlDoc =  Get-Content "source.extension.vsixmanifest"
+Write-Host 'reading'
+$xmlDoc | Format-List *
+$node=$xmlDoc.PackageManifest.Metadata.Identity
 
-Compress-Archive -DestinationPath .\a -Path GeneratorFromDB\*
+$node=$node.SetAttribute("Version", "2023.3.11.1502")
+
+$xmlDoc.OuterXml | Out-File $FileLocation
+# $xmlDoc.Save(source.extension.vsixmanifest1)
+New-item –ItemType "directory" GeneratorFromDBTemp
+Copy-Item -Path GeneratorFromDB\* -Destination GeneratorFromDBTemp\ -Force -Recurse
+
+push-location 
+cd ..
+dotnet clean
+pop-location
+
+Write-Host "copying files"
+copy-item -Path ..\ExampleModels\ 		-Destination GeneratorFromDBTemp\ExampleModels\ 		-Force -Recurse
+copy-item -Path ..\ExampleControllers\ 	-Destination GeneratorFromDBTemp\ExampleControllers\ 	-Force -Recurse
+copy-item -Path ..\ExampleContext\	 	-Destination GeneratorFromDBTemp\ExampleContext\ 		-Force -Recurse
+copy-item -Path ..\ExampleWebAPI\ 		-Destination GeneratorFromDBTemp\ExampleWebAPI\ 		-Force -Recurse
+
+Write-Host "modify .cs files"
+push-location 
+cd GeneratorFromDBTemp
+gci *.cs -r | % { 
+	$content  = Get-Content $_.FullName 
+	$newContent = $content -replace 'Example','$safeprojectname$'
+	if ($content -ne $newContent) {
+		Set-Content -Path  $_.FullName -Value $newContent
+		Write-Host 'replacing ' $_.FullName 
+		
+	}
+		
+}
+Write-Host "modify .csproj files"
+gci *.csproj -r | % { 
+	$content  = Get-Content $_.FullName 
+	$newContent = $content -replace 'Example','$ext_safeprojectname$'
+	if ($content -ne $newContent) {
+		Set-Content -Path  $_.FullName -Value $newContent
+		Write-Host 'replacing ' $_.FullName 
+		
+	}
+		
+}
+
+pop-location
+ 
+
+Compress-Archive -DestinationPath .\a -Path GeneratorFromDBTemp\*
 Remove-Item .\GeneratorFromDB.zip
 Remove-Item .\ProjectTemplates\GeneratorFromDB.zip
 Copy-Item .\a.zip .\ProjectTemplates\GeneratorFromDB.zip
