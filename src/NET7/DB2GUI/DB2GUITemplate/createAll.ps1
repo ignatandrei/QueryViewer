3bc677ed60d66d6f3cd4b1dd76fed616a9aa9192
Andrei Ignat <ignatandrei@yahoo.com>
2023-03-19 09:34:14 +0200

    adding react template

--- src/NET7/DB2GUI/DB2GUITemplate/createAll.ps1
+++ src/NET7/DB2GUI/DB2GUITemplate/createAll.ps1
@@ -11,7 +11,7 @@ $xml = [xml]( Get-Content "MyTemplate.vstemplate")
 
 
 $node= $xml.VSTemplate.TemplateContent.Project
-gci *.* -r -Exclude *.csproj,*.vstemplate, __TemplateIcon.ico | % { 
+gci *.* -r -Exclude *.*sproj,*.vstemplate, __TemplateIcon.ico | % { 
 	$rel = Resolve-Path -Relative $_
 	$rel = $rel.replace( ".\","")
 	$newelement = $xml.CreateElement("ProjectItem")
@@ -71,23 +71,26 @@ dotnet clean
 pop-location
 
 Write-Host "copying files"
-copy-item -Path ..\ExampleModels\* 			-Destination GeneratorFromDBTemp\ExampleModels\ 		-Force -Recurse
-copy-item -Path ..\ExampleControllers\* 	-Destination GeneratorFromDBTemp\ExampleControllers\ 	-Force -Recurse
-copy-item -Path ..\ExampleContext\*	 		-Destination GeneratorFromDBTemp\ExampleContext\ 		-Force -Recurse
-copy-item -Path ..\ExampleWebAPI\* 			-Destination GeneratorFromDBTemp\ExampleWebAPI\ 		-Force -Recurse
-copy-item -Path ..\GeneratorPowershell\* 	-Destination GeneratorFromDBTemp\GeneratorPowershell\ 	-Force -Recurse
-copy-item -Path ..\GeneratorFromDB\* 		-Destination GeneratorFromDBTemp\GeneratorFromDB\ 		-Force -Recurse
+copy-item -Path ..\ExampleModels\* 			-Destination GeneratorFromDBTemp\ExampleModels\ 							-Force -Recurse
+copy-item -Path ..\ExampleControllers\* 	-Destination GeneratorFromDBTemp\ExampleControllers\ 						-Force -Recurse
+copy-item -Path ..\ExampleContext\*	 		-Destination GeneratorFromDBTemp\ExampleContext\ 							-Force -Recurse
+copy-item -Path ..\ExampleWebAPI\* 			-Destination GeneratorFromDBTemp\ExampleWebAPI\ 							-Force -Recurse
+copy-item -Path ..\GeneratorPowershell\* 	-Destination GeneratorFromDBTemp\GeneratorPowershell\ 						-Force -Recurse
+copy-item -Path ..\GeneratorFromDB\* 		-Destination GeneratorFromDBTemp\GeneratorFromDB\ 							-Force -Recurse
+copy-item -Path ..\examplecrats\* 		    -Destination GeneratorFromDBTemp\examplecrats\ 	-Exclude node_modules		-Force -Recurse
+copy-item -Path ..\GeneratorCRA\* 		    -Destination GeneratorFromDBTemp\GeneratorCRA\ 								-Force -Recurse
 
 
-Write-Host "modify .cs files"
+
 push-location 
 cd GeneratorFromDBTemp
 
-Write-Host "delete bin and obj"
+Write-Host "delete remaining folders"
 Get-ChildItem  -Directory -Recurse -Filter "bin" | Remove-Item -Recurse
 Get-ChildItem  -Directory -Recurse -Filter "obj" | Remove-Item -Recurse
+Get-ChildItem  -Directory -Recurse -Filter "node_modules" | Remove-Item -Recurse
 
-
+Write-Host "modify .cs files"
 gci *.cs -r | % { 
 	$content  = Get-Content $_.FullName 
 	$newContent = $content -replace 'Example','$safeprojectname$'
@@ -113,11 +116,12 @@ gci connectionDetails.txt -r | % {
 }
 
 
-Write-Host "modify .csproj files"
-gci *.csproj -r | % { 
+Write-Host "modify .*sproj files"
+gci *.*sproj -r | % { 
 	$content  = Get-Content $_.FullName 
 	$newContent = $content
 	$newContent = $newContent -replace 'Example','$ext_safeprojectname$.'
+	$newContent = $newContent -replace 'example','$ext_safeprojectname$.'
 	$newContent = $newContent -replace "..\\GeneratorFromDB\\GeneratorFromDB.csproj",'..\$ext_specifiedsolutionname$.GeneratorFromDB\$ext_specifiedsolutionname$.GeneratorFromDB.csproj'
 	if ($content -ne $newContent) {
 		Set-Content -Path  $_.FullName -Value $newContent
@@ -138,7 +142,8 @@ AddFiles "ExampleContext"
 AddFiles "ExampleWebAPI"
 AddFiles "GeneratorPowershell"
 AddFiles "GeneratorFromDB"
-
+AddFiles "examplecrats"
+AddFiles "GeneratorCRA"
 
 
 Compress-Archive -DestinationPath .\a -Path GeneratorFromDBTemp\*
