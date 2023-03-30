857e1ca48ae9aad7ccdd507fea3f6b2d3d18b1da
Andrei Ignat <ignatandrei@yahoo.com>
2023-03-30 19:50:44 +0300

    generating react

--- src/NET7/DB2GUI/GeneratorPowershell/create.ps1
+++ src/NET7/DB2GUI/GeneratorPowershell/create.ps1
@@ -25,18 +25,58 @@ dotnet tool list -g
 Write-Host "search for CRA in $ProjectCRA"
 
 $projectsFull = gci -Path $ProjectCRA -File  -Filter *.csproj | Select-Object  Fullname | Select-Object -First 1
-
-
 $project = $projectsFull.Fullname
-Write-Host "found project $project "
+
+
 $pathToModels = "../examplecrats/src/Admin/Generated/Models/"+$nameContext
 $pathToContext= "../examplecrats/src/Admin/Generated/DB/"+$nameContext
 Write-Host "pathToModels" $pathToModels
 
-# https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
-dotnet ef dbcontext scaffold $connection --data-annotations  -p $project -s $project $provider -v -f --no-pluralize --no-onconfiguring --use-database-names  --context $nameContext  --context-namespace Generated --namespace Generated --context-dir $pathToContext --output-dir $pathToModels  --prefix-output --force --json #--no-build
+function Generate{
 
- #return
+    param (
+        [Parameter(Mandatory)]
+        [string]$nameWhatToGenerate,
+        [Parameter(Mandatory)]
+        [string]$paramPathToGenerated,
+       [Parameter(Mandatory)]
+        [string]$paramProjectToCompile
+    )
+
+    
+    Write-Output "Generate compile " $paramProjectToCompile "and output to " $paramPathToGenerated
+
+    $pathFolder = [System.IO.Path]::GetDirectoryName($paramProjectToCompile)
+    Write-Output "start copying folder templates to " $pathFolder
+    $pathTemplates = [System.IO.Path]::Combine($pathFolder,"CodeTemplates")
+    Write-Host "verify " $pathTemplates 
+    if (Test-Path -Path $pathTemplates) {
+        Write-Host "delete " $pathTemplates         
+        Remove-Item -LiteralPath $pathTemplates -Force -Recurse
+    }
+    
+    #New-item –itemtype directory $pathTemplates
+    $pathTemplatesToCopy = [System.IO.Path]::Combine($pathFolder,"GeneratorTemplates",$nameWhatToGenerate,"CodeTemplates")
+    Write-Host "copy from " $pathTemplatesToCopy
+    Copy-Item -Path $pathTemplatesToCopy -Destination $pathFolder -Recurse
+
+    $pathToModels = $paramPathToGenerated + "Generated/Models/"+$nameContext
+    $pathToContext= $paramPathToGenerated + "Generated/DB/"+$nameContext
+
+
+
+    # https://docs.microsoft.com/ro-ro/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
+    dotnet ef dbcontext scaffold $connection --data-annotations  -p $project -s $project $provider -v -f --no-pluralize --no-onconfiguring --use-database-names  --context $nameContext  --context-namespace Generated --namespace Generated --context-dir $pathToContext --output-dir $pathToModels  --prefix-output --force --json #--no-build
+
+
+}
+
+# Generate "examplecrats"  "../examplecrats/src/Admin/Generated/" $project
+
+Generate "ExampleModels"  "../ExampleModels/Generated/" $project
+
+ 
+return
 
 Write-Host "search for csproj model in $projectModels"
 
