27e84655279588a8f80effe1d4d257abc51f48c9
Andrei Ignat <ignatandrei@yahoo.com>
2023-12-09 20:05:48 +0200

    added aspire host

---
+++ src/NET8/DB2GUI/DB2Gui.AspireHost/Program.cs
@@ -0,0 +1,15 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+//var rb= builder.AddSqlServerContainer("Db2Gui");
+//var db = rb.AddDatabase("TestDB");
+//var apiservice = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+
+//builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+//    .WithReference(cache)
+//    .WithReference(apiservice);
+
+builder.Build().Run();
+
+//var res = db.Resource;
+//var cn = res.GetConnectionString();
+//Console.WriteLine(cn);
